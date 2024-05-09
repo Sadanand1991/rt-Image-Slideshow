63f3b43776fcea7ebc585d5a8928d9ee107a4c2a
Sadanand Lonari <lonari.sadanand@gmail.com>
2024-05-09 23:32:57 +0530

    Code improvement based on WPCS.

---
+++ admin/class-slideshow-settings-page.php
@@ -0,0 +1,227 @@
+<?php
+/**
+ * SLIDESHOW_SETTINGS
+ *
+ * @package slideshow-settings
+ */
+
+define( 'SLIDESHOW_SETTINGS', 'slideshow-settings' );
+
+/**
+ * Slideshow settings page class.
+ */
+class Slideshow_Settings_Page {
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		add_action( 'admin_menu', array( $this, 'on_admin_menu' ) );
+		add_action( 'admin_post_save_slideshow_settings', array( $this, 'setting_on_save_changes' ) );
+	}
+
+	/**
+	 * Extend the admin menu.
+	 */
+	public function on_admin_menu() {
+		$this->pagehook = add_options_page( 'Slideshow Settings', 'Slideshow Settings', 'manage_options', SLIDESHOW_SETTINGS, array( $this, 'on_show_page' ) );
+		add_action( 'load-' . $this->pagehook, array( $this, 'on_load_page' ) );
+	}
+
+	/**
+	 * Load admin page.
+	 */
+	public function on_load_page() {
+		wp_enqueue_script( 'common' );
+		wp_enqueue_script( 'wp-lists' );
+		wp_enqueue_script( 'postbox' );
+		wp_enqueue_script( 'jquery' );
+		wp_enqueue_script( 'jquery-ui-core' );
+		wp_enqueue_script( 'jquery-ui-tabs' );
+		wp_enqueue_script( 'jquery-ui-sortable' );
+		wp_enqueue_script( 'wp-color-picker' );
+		wp_enqueue_script( 'thickbox' );
+		wp_enqueue_script( 'media-upload' );
+		wp_enqueue_style( 'thickbox' );
+		wp_enqueue_style( 'wp-color-picker' );
+		wp_enqueue_style( 'rt-admin-css', SLIDESHOW_PATH . 'admin/css/slide_admin.css' );
+		wp_enqueue_script( 'rt-admin-js', SLIDESHOW_PATH . 'admin/js/slide_admin.js', array(), null, true );
+
+		if ( function_exists( 'wp_enqueue_media' ) && ! did_action( 'wp_enqueue_media' ) ) {
+			wp_enqueue_media();
+		}
+
+		add_meta_box( 'slideshow_settings', 'Slideshow Settings', array( $this, 'slideshow_settings' ), $this->pagehook, 'normal', 'core' );
+	}
+
+	/**
+	 * Show admin page.
+	 */
+	public function on_show_page() {
+		$data = array();
+		?>
+		<div id="slideshow-settings-metaboxes" class="wrap">
+			<?php echo esc_html( get_admin_page_title() ); ?>
+			<form action="admin-post.php" method="post">
+				<?php wp_nonce_field( 'slideshow-settings-metaboxes' ); ?>
+				<?php wp_nonce_field( 'closedpostboxes', 'closedpostboxesnonce', false ); ?>
+				<?php wp_nonce_field( 'meta-box-order', 'meta-box-order-nonce', false ); ?>
+				<input type="hidden" name="action" value="save_slideshow_settings" />
+				<div id="poststuff" class="metabox-holder has-right-sidebar">
+					<div id="side-info-column" class="inner-sidebar">
+						<div class="postbox">
+							<div class="inside">
+								<input type="hidden" name="HTTP_REFERER" value="<?php echo esc_url( wp_get_referer() ); ?>" />
+								<input type="hidden" name="theme_options_nonce" value="<?php echo esc_attr( wp_create_nonce( 'input' ) ); ?>" />
+								<input type="submit" class="button button-primary button-large" value="<?php esc_attr_e( 'Save Slider', 'rt-image-slideshow' ); ?>" />
+							</div>
+						</div>
+						<?php do_meta_boxes( $this->pagehook, 'side', $data ); ?>
+					</div>
+					<div id="post-body" class="has-sidebar">
+						<div id="post-body-content" class="has-sidebar-content">
+							<?php do_meta_boxes( $this->pagehook, 'normal', $data ); ?>
+							<?php do_meta_boxes( $this->pagehook, 'additional', $data ); ?>
+						</div>
+					</div>
+					<br class="clear"/>
+				</div>
+			</form>
+		</div>
+		<script type="text/javascript">
+			jQuery(document).ready(function($) {
+				jQuery('.if-js-closed').removeClass('if-js-closed').addClass('closed');
+				postboxes.add_postbox_toggles('<?php echo esc_js( $this->pagehook ); ?>');
+			});
+		</script>
+		<?php
+	}
+
+	/**
+	 * Save settings.
+	 */
+	public function setting_on_save_changes() {
+		if ( ! current_user_can( 'manage_options' ) ) {
+			wp_die( esc_html__( 'Cheatin&#8217; uh?', 'rt-image-slideshow' ) );
+		}
+
+		check_admin_referer( 'slideshow-settings-metaboxes' );
+
+		update_option( 'slideshow_settings', wp_unslash( wp_json_encode( $_POST['slider_image'] ) ) );
+		wp_safe_redirect( esc_url_raw( wp_get_referer() ) );
+		exit;
+	}
+
+	/**
+	 * Display slideshow settings.
+	 *
+	 * @param array $data Additional data.
+	 */
+	public function slideshow_settings( $data ) {
+		$slideshow_images = unserialize( get_option( 'slideshow_settings' ) );
+		?>
+		<div id="rt-event_venue" class="field field_type-repeater rt-tab_group-show" data-field_name="event_venue" data-field_key="" data-field_type="repeater">
+			<p class="label"><label for="rt-field-event_venue"><?php esc_html_e( 'Slider Images', 'rt-image-slideshow' ); ?></label></p>
+			<div class="repeater empty" data-min_rows="0" data-max_rows="999">
+				<table class="widefat rt-input-table row_layout" data-input-name="slider_image">
+					<tbody class="">
+					<?php
+					$i = 1;
+					if ( ! empty( $slideshow_images ) ) {
+						foreach ( $slideshow_images as $slideshow_image ) {
+							if ( ! empty( $slideshow_image ) ) {
+								?>
+								<tr class="row">
+									<td class="order"><?php echo esc_html( $i ); ?></td>
+									<td class="rt_input-wrap">
+										<table class="widefat rt_input">
+											<tbody>
+												<tr class="field sub_field field_type-image" data-field_type="image" data-field_key="" data-field_name="slider_image">
+													<td class="label">
+														<label><?php esc_html_e( 'Image', 'rt-image-slideshow' ); ?></label>
+														<span class="sub-field-instructions"></span>
+													</td>
+													<td>
+														<div class="inner">
+															<div class="rt-image-uploader clearfix" data-preview_size="thumbnail" data-library="all">
+																<input class="rt-image-value" type="hidden" name="slider_image[]" value="<?php echo esc_url( $slideshow_image ); ?>">
+																<div class="my-image">
+																	<div class="hover">
+																		<ul class="bl">
+																			<li><a class="rt-button-delete ir" href="#"><?php esc_html_e( 'Remove', 'rt-image-slideshow' ); ?></a></li>
+																		</ul>
+																	</div>
+																	<img class="rt-image-image" src="<?php echo esc_url( $slideshow_image ); ?>" alt="">
+																</div>
+															</div>
+														</div>
+													</td>
+												</tr>
+											</tbody>
+										</table>
+									</td>
+									<td class="remove">
+										<a class="rt-button-add add-row-before" href="#" style="margin-top: -27.5px;"></a>
+										<a class="rt-button-remove" href="#"></a>
+									</td>
+								</tr>
+								<?php
+								++$i;
+							}
+						}
+					}
+					?>
+					<tr class="row-clone">
+						<td class="order"></td>
+						<td class="rt_input-wrap">
+							<table class="widefat rt_input">
+								<tbody>
+									<tr class="field sub_field field_type-image" data-field_type="image" data-field_key="" data-field_name="slider_image">
+										<td class="label">
+											<label><?php esc_html_e( 'Image', 'rt-image-slideshow' ); ?></label>
+											<span class="sub-field-instructions"></span>
+										</td>
+
+										<td>
+											<div class="inner">
+												<div class="rt-image-uploader clearfix" data-preview_size="thumbnail" data-library="all">
+														<input class="rt-image-value" type="hidden" name="slider_image[]" value="">
+														<div class="has-image">
+															<div class="hover">
+																<ul class="bl">
+																	<li><a class="rt-button-delete ir" href="#"><?php esc_html_e( 'Remove', 'rt-image-slideshow' ); ?></a></li>
+																</ul>
+															</div>
+															<img class="rt-image-image" src="" alt="">
+														</div>
+														<div class="no-image">
+															<p>
+																<?php esc_html_e( 'No image selected', 'rt-image-slideshow' ); ?> <input type="button" class="button add-image upload_image_button" value="<?php esc_attr_e( 'Add Image', 'rt-image-slideshow' ); ?>">
+															</p>
+														</div>
+												</div>
+											</div>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</td>
+						<td class="remove">
+							<a class="rt-button-add add-row-before" href="#" style="margin-top: -27.5px;"></a>
+							<a class="rt-button-remove" href="#"></a>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			<ul class="hl clearfix repeater-footer">
+				<li class="right">
+					<a href="#" class="add-row-end button button-primary button-medium"><?php esc_html_e( 'Add Row', 'rt-image-slideshow' ); ?></a>
+				</li>
+			</ul>
+			</div>
+		</div>
+		<?php
+	}
+}
+
+$subscribe_settings_page       = new Slideshow_Settings_Page();
+$GLOBALS['slideshow_settings'] = unserialize( get_option( 'slideshow_settings' ) );
--- admin/js/slide_admin.js
+++ admin/js/slide_admin.js
@@ -1,181 +1,197 @@
 /**
-* Theme Admin JS
-*/
+ * Theme Admin JS
+ */
 
-jQuery(document).ready(function($) {
-        
-    var _orig_send_attachment = wp.media.editor.send.attachment;    
+jQuery( document ).ready(
+	function ($) {
 
-    $('body').on('click', '.upload_image_button', function(e) {
-        var send_attachment_bkp = wp.media.editor.send.attachment;
-        var frame = wp.media( {
-            title       : 'Image Uploader',
-            multiple    : false,
-            button      : { text : 'Insert Image' },
-            editable:   false,
-            allowLocalEdits: false,
-            displaySettings: false,
-            displayUserSettings: false,
-            filterable: 'uploaded',
-            library:   wp.media.query(),
-        } );
-        var button = $(this).parents('.inner').find('input.rt-image-value');
-        var img = $(this).parents('.inner').find('img');
-        $(this).parents('.rt-image-uploader').addClass('active');
-        _custom_media = true;
-        wp.media.editor.send.attachment = function(props, attachment){
-            if ( _custom_media ) {
-                button.val(attachment.url);
-                img.attr('src', attachment.url);
-                _custom_media = false;
-            } else {
-              return _orig_send_attachment.apply( this, [props, attachment] );
-            };
-        }
+		var _orig_send_attachment = wp.media.editor.send.attachment;
 
-        wp.media.editor.open(button);
-        return false;
-    });
-    
-    $('body').on('click', '.rt-image-uploader .rt-button-delete', function( e ){
-        e.preventDefault();
-        $(this).parents('.rt-image-uploader').removeClass('active').find('input.rt-image-value').val();    
-    });
-    
-    $(document).on('click', '.repeater .repeater-footer .add-row-end', function( e ){
-        e.preventDefault();
-        var $this = $(this);
-        sortElements.set({ $el : $(this).closest('.repeater') }).add( false, $this);
-        $(this).blur();
-    });
-    
+		$( 'body' ).on(
+			'click',
+			'.upload_image_button',
+			function (e) {
+				var send_attachment_bkp = wp.media.editor.send.attachment;
+				var frame               = wp.media(
+					{
+						title       : 'Image Uploader',
+						multiple    : false,
+						button      : { text : 'Insert Image' },
+						editable:   false,
+						allowLocalEdits: false,
+						displaySettings: false,
+						displayUserSettings: false,
+						filterable: 'uploaded',
+						library:   wp.media.query(),
+					}
+				);
+				var button              = $( this ).parents( '.inner' ).find( 'input.rt-image-value' );
+				var img                 = $( this ).parents( '.inner' ).find( 'img' );
+				$( this ).parents( '.rt-image-uploader' ).addClass( 'active' );
+				_custom_media                   = true;
+				wp.media.editor.send.attachment = function (props, attachment) {
+					if ( _custom_media ) {
+						button.val( attachment.url );
+						img.attr( 'src', attachment.url );
+						_custom_media = false;
+					} else {
+						return _orig_send_attachment.apply( this, [props, attachment] );
+					};
+				}
 
-    $(document).on('click', '.repeater td.remove .add-row-before', function( e ){
-        e.preventDefault();
-        sortElements.set({ $el : $(this).closest('.repeater') }).add(false, $(this).closest('tr') );
-        $(this).blur();
-    });
-    
-    
-    $(document).on('click', '.repeater td.remove .rt-button-remove', function( e ){
-        e.preventDefault();
-        sortElements.set({ $el : $(this).closest('.repeater') }).remove( $(this).closest('tr') );
-        $(this).blur();
-    });
-    
-    $( "#rt-event_venue .repeater" ).sortable({
-        items  : 'tr.row',
-        handle : 'td.order',
-        stop: function() {
-            sortElements.render();
-        }
-    });
-    
-    var sortElements = {
-        $el : null,	
-        o : {},
-        set : function( o ){
-            // merge in new option
-            $.extend( this, o );
-            // add row_count
-            this.o.row_count = this.$el.find('> table > tbody > tr.row').length;
-            // return this for chaining
-            return this;
-        },
-        
-        init : function(){
-	    this.render();
-        },
-        
-        render: function() {
-            // update row_count
-            console.log(this);
-            this.o.row_count = this.$el.find('> table > tbody > tr.row').length;
-            var attrName = this.$el.find('> table').attr('data-input-name');
-            // update order numbers
-            this.$el.find('> table > tbody > tr.row').each(function(i){
-                $(this).children('td.order').html( i+1 );
-                $(this).find('input.rt-image-value').attr('name', attrName+'['+(i+1)+']');
-            });
+				wp.media.editor.open( button );
+				return false;
+			}
+		);
 
+		$( 'body' ).on(
+			'click',
+			'.rt-image-uploader .rt-button-delete',
+			function ( e ) {
+				e.preventDefault();
+				$( this ).parents( '.rt-image-uploader' ).removeClass( 'active' ).find( 'input.rt-image-value' ).val();
+			}
+		);
 
-            // empty?
-            if( this.o.row_count == 0 )
-            {
-                this.$el.addClass('empty');
-            }
-            else
-            {
-                this.$el.removeClass('empty');
-            }
+		$( document ).on(
+			'click',
+			'.repeater .repeater-footer .add-row-end',
+			function ( e ) {
+				e.preventDefault();
+				var $this = $( this );
+				sortElements.set( { $el : $( this ).closest( '.repeater' ) } ).add( false, $this );
+				$( this ).blur();
+			}
+		);
 
+		$( document ).on(
+			'click',
+			'.repeater td.remove .add-row-before',
+			function ( e ) {
+				e.preventDefault();
+				sortElements.set( { $el : $( this ).closest( '.repeater' ) } ).add( false, $( this ).closest( 'tr' ) );
+				$( this ).blur();
+			}
+		);
 
-            // row limit reached
-            if( this.o.row_count >= this.o.max_rows )
-            {
-                this.$el.addClass('disabled');
-                this.$el.find('> .repeater-footer .rt-button').addClass('disabled');
-            }
-            else
-            {
-                this.$el.removeClass('disabled');
-                this.$el.find('> .repeater-footer .rt-button').removeClass('disabled');
-            }
-        },
-        
-        add: function($before, $this) {
-                console.log($this);
-                var total_elements = parseInt($this.parents('.repeater').find('.row').length);
-                total_elements = total_elements+1;
-                
-                var new_field_html = this.$el.find('> table > tbody > tr.row-clone').html().replace(/(=["]*[\w-\[\]]*?)(rtcloneindex)/g, '$1' + total_elements);
-               
-//                new_field_html = this.$el.find('> table > tbody > tr.row-clone').find('input').attr('name', 'slider_image['+total_elements+']');
-                var new_field = $('<tr class="row"></tr>').append( new_field_html );
-                new_field.find('input.rt-image-value').attr('name','slider_image['+total_elements+']');
-                if( ! $before )
-                {
-                    $before = this.$el.find('> table > tbody > .row-clone');
-                }
+		$( document ).on(
+			'click',
+			'.repeater td.remove .rt-button-remove',
+			function ( e ) {
+				e.preventDefault();
+				sortElements.set( { $el : $( this ).closest( '.repeater' ) } ).remove( $( this ).closest( 'tr' ) );
+				$( this ).blur();
+			}
+		);
 
-                $before.before( new_field );
-                
+		$( "#rt-event_venue .repeater" ).sortable(
+			{
+				items  : 'tr.row',
+				handle : 'td.order',
+				stop: function () {
+					sortElements.render();
+				}
+			}
+		);
 
-                // trigger mouseenter on parent repeater to work out css margin on add-row button
-                this.$el.closest('tr').trigger('mouseenter');
+		var sortElements = {
+			$el : null,
+			o : {},
+			set : function ( o ) {
+				// merge in new option
+				$.extend( this, o );
+				// add row_count
+				this.o.row_count = this.$el.find( '> table > tbody > tr.row' ).length;
+				// return this for chaining
+				return this;
+			},
 
+			init : function () {
+				this.render();
+			},
 
-                // update order
-                this.render();
-                
-//                console.log(total_elements);
-//                $(this).parents('.repeater').find('.row-clone td.order').text(total_elements);
-//                var insData = '<tr class="row">';
-//                insData += $(this).parents('.repeater').find('.row-clone').html();
-//                insData += '</tr>';
-//                $(this).parents('.row').before(insData);
-        },
-        
-        remove: function($tr) {
-            
-            var _this = this;
-            
-            $tr.addClass('rt-remove-item');
-            setTimeout(function(){
+			render: function () {
+				// update row_count
+				console.log( this );
+				this.o.row_count = this.$el.find( '> table > tbody > tr.row' ).length;
+				var attrName     = this.$el.find( '> table' ).attr( 'data-input-name' );
+				// update order numbers
+				this.$el.find( '> table > tbody > tr.row' ).each(
+					function (i) {
+						$( this ).children( 'td.order' ).html( i + 1 );
+						$( this ).find( 'input.rt-image-value' ).attr( 'name', attrName + '[' + (i + 1) + ']' );
+					}
+				);
 
-                    $tr.remove();
+				// empty?
+				if ( this.o.row_count == 0 ) {
+					this.$el.addClass( 'empty' );
+				} else {
+					this.$el.removeClass( 'empty' );
+				}
 
+				// row limit reached
+				if ( this.o.row_count >= this.o.max_rows ) {
+					this.$el.addClass( 'disabled' );
+					this.$el.find( '> .repeater-footer .rt-button' ).addClass( 'disabled' );
+				} else {
+					this.$el.removeClass( 'disabled' );
+					this.$el.find( '> .repeater-footer .rt-button' ).removeClass( 'disabled' );
+				}
+			},
 
-                    // trigger mouseenter on parent repeater to work out css margin on add-row button
-                    _this.$el.closest('tr').trigger('mouseenter');
+			add: function ($before, $this) {
+				console.log( $this );
+				var total_elements = parseInt( $this.parents( '.repeater' ).find( '.row' ).length );
+				total_elements     = total_elements + 1;
 
+				var new_field_html = this.$el.find( '> table > tbody > tr.row-clone' ).html().replace( /(=["]*[\w-\[\]]*?)(rtcloneindex)/g, '$1' + total_elements );
 
-                    // render
-                    _this.render();
+				// new_field_html = this.$el.find('> table > tbody > tr.row-clone').find('input').attr('name', 'slider_image['+total_elements+']');
+				var new_field = $( '<tr class="row"></tr>' ).append( new_field_html );
+				new_field.find( 'input.rt-image-value' ).attr( 'name','slider_image[' + total_elements + ']' );
+				if ( ! $before ) {
+					$before = this.$el.find( '> table > tbody > .row-clone' );
+				}
 
-            }, 400);
-            
-        }
-    };
+				$before.before( new_field );
 
-});
+				// trigger mouseenter on parent repeater to work out css margin on add-row button
+				this.$el.closest( 'tr' ).trigger( 'mouseenter' );
+
+				// update order
+				this.render();
+
+				// console.log(total_elements);
+				// $(this).parents('.repeater').find('.row-clone td.order').text(total_elements);
+				// var insData = '<tr class="row">';
+				// insData += $(this).parents('.repeater').find('.row-clone').html();
+				// insData += '</tr>';
+				// $(this).parents('.row').before(insData);
+			},
+
+			remove: function ($tr) {
+
+				var _this = this;
+
+				$tr.addClass( 'rt-remove-item' );
+				setTimeout(
+					function () {
+
+						$tr.remove();
+
+						// trigger mouseenter on parent repeater to work out css margin on add-row button
+						_this.$el.closest( 'tr' ).trigger( 'mouseenter' );
+
+						// render
+						_this.render();
+
+					},
+					400
+				);
+
+			}
+		};
+
+	}
+);
